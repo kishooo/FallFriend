62672fe927248ac9f6eb66586148d5cece92d267
kishooo <47245011+kishooo@users.noreply.github.com>
2021-01-21 21:21:15 +0200

    MainMenu UI added

---
+++ Assets/Scripts/MainMenu1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void option()
+    {
+        SceneManager.LoadScene("option");
+    }
+}
